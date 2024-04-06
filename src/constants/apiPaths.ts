551a93266871f3ced6e628837bdb08e85f4aee3d
Mariia Zaiets <mariia_zaiets@epam.com>
2024-04-06 17:35:31 +0200

    Integration with PostreSQL RDS. Implement createProduct logic

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
     "https://igqfikc3n1.execute-api.us-east-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://igqfikc3n1.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
